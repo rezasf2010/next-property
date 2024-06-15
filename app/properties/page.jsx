9a2b74b4529a3f85c8fe3e62376c91b0e2c84aa5
Reza Saberifard <rezasf2010@gmail.com>
2024-06-15 13:58:10 +0330

    End of Section 8

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,5 +1,6 @@
 import PorpertyCard from '@/components/PorpertyCard';
 import { fetchProperties } from '@/utils/requests';
+import ProppertySearchFrom from '@/components/ProppertySearchFrom';
 
 
 const PropertiesPage = async () => {
@@ -8,7 +9,13 @@ const PropertiesPage = async () => {
   // Sort properties by date
   properties.sort((a,b) => new Date(b.createdAt) - new Date(a.createdAt))
 
-  return (
+  return (<>
+    <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+            <ProppertySearchFrom />
+        </div>
+    </section>
+
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
         {properties.length === 0 ? 
@@ -21,7 +28,8 @@ const PropertiesPage = async () => {
         )}
       </div>
     </section>
-  )
+
+  </>)
 }
 
 export default PropertiesPage
--- components/PorpertyCard.jsx
+++ components/PorpertyCard.jsx
@@ -27,6 +27,7 @@ const PorpertyCard = ({ property }) => {
       height={0}
       sizes="100vw"
       className='w-full h-auto rounded-t-xl'
+      priority={true}
     />
     <div className="p-4">
       <div className="text-left md:text-center lg:text-left mb-6">
