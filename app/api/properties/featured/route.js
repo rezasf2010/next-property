1b8930610fdb7fe47a6989c6b1f84ff8680a14ae
Reza Saberifard <rezasf2010@gmail.com>
2024-06-16 19:25:07 +0330

    reverting to previous version

--- app/api/properties/featured/route.js
+++ app/api/properties/featured/route.js
@@ -3,7 +3,7 @@ import Property from "@/models/Property";
 
 export const dynamic = 'force-dynamic';
 
-// GET /api/properties
+// GET /api/properties/featerd
 export const GET = async (request) => {
     try {
         await connectDB();
